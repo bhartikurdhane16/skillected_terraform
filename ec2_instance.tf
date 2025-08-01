5dc1633b7ea9f6d9b55172eab32cd6924dabab50
bharti <bharti@gmail.com>
2025-08-01 18:32:38 +0530

    --- Auto Git Commit ---

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -18,7 +18,7 @@ provider "aws" {
   region = "ap-south-1"               #region where want to create instance
   shared_config_files      = ["/root/.aws/config"]          #iamuser profile detalis 
   shared_credentials_files = ["/root/.aws/credentials"]
-  profile                  = "terraform_user"
+  profile                  = "terraform-user"
 }
 
 
