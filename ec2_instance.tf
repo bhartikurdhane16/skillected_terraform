4a002e73f9c8dbc3f1c0ef25005a537dc41c75dc
bharti <bharti@gmail.com>
2025-08-01 18:11:21 +0530

    --- Auto Git Commit ---

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "xyz" {
-    ami_id= ami-0f918f7e67a3323f0"
+    ami_id= "ami-0f918f7e67a3323f0"
     instance_type= "t2.micro"
     key_name= "server1_key"
   }
